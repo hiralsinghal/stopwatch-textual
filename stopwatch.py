9048434d34ef2f38ac88cc5b29f0857198c8b438
hiralsinghal <singhal.hiral@gmail.com>
2026-02-17 02:00:11 +0530

    function for adding nd removing stopwatches

--- stopwatch.py
+++ stopwatch.py
@@ -63,12 +63,22 @@ class StopwatchApp(App):
         ("d","toggle_dark","Toggle dark mode"),
         ("a","add_stopwatch","Add"),
         ("r","remove_stopwatch","Remove")
-                ]
+    ]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
-        yield VerticalScroll(Stopwatch())
+        yield VerticalScroll(Stopwatch(),Stopwatch(),Stopwatch(),id="timers")
+
+    def action_add_stopwatch(self)->None:
+        new_stopwatch=Stopwatch()
+        self.query_one("#timers").mount(new_stopwatch)
+        new_stopwatch.scroll_visible()
+
+    def action_remove_stopwatch(self)->None:
+        timers = self.query("Stopwatch")
+        if timers:
+            timers.last().remove()
 
     def action_toggle_dark(self):
         self.theme = (
