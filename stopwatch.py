75373369923314f4ab7583ebfbc5502068feaabc
hiralsinghal <singhal.hiral@gmail.com>
2026-02-17 01:54:30 +0530

    added more functionality so that when buttons are pressed it sends a cmd

--- stopwatch.py
+++ stopwatch.py
@@ -6,25 +6,64 @@ from textual.containers import HorizontalGroup,VerticalScroll
 from textual.reactive import reactive
 
 class TimeDisplay(Digits):
+
     start_time=reactive(monotonic)
     time=reactive(0.0)
+    total=reactive(0.0)
 
     def on_mount(self)-> None:
-        self.set_interval(1/60,self.update_time)
+        self.update_timer=self.set_interval(1/60,self.update_time,pause=True)
     
     def update_time(self) ->None:
-        self.time = monotonic() - self.start_time
+        self.time = self.total + (monotonic() - self.start_time)
+
+    def watch_time(self,time:float)->None:
+        minutes, seconds = divmod(time,60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
+
+    def start(self)->None:
+        self.start_time=monotonic()
+        self.update_timer.resume()
+
+    def stop(self)->None:
+        self.update_timer.pause()
+        self.total += monotonic()-self.start_time
+        self.time=self.total
+
+    def reset(self)->None:
+        self.total=0
+        self.time=0
 
 class Stopwatch(HorizontalGroup):
+
+    def on_button_pressed(self,event:Button.Pressed)->None:
+        button_id = event.button.id
+        time_display = self.query_one(TimeDisplay)
+
+        if button_id=="start":
+            time_display.start()
+            self.add_class("Started")
+        elif button_id=="stop":
+            time_display.stop()
+            self.remove_class("Started")
+        elif button_id=="reset":
+            time_display.reset()
+
     def compose(self) -> ComposeResult:
         yield Button("Start",id="start",variant="success")
         yield Button("Stop", id="stop",variant="error")
         yield Button("Reset",id="reset")
-        yield TimeDisplay("00:00:00.00")
+        yield TimeDisplay()
 
 
 class StopwatchApp(App):
-    BINDINGS = [("d","toggle_dark","Toggle dark mode")]
+
+    BINDINGS = [
+        ("d","toggle_dark","Toggle dark mode"),
+        ("a","add_stopwatch","Add"),
+        ("r","remove_stopwatch","Remove")
+                ]
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -38,4 +77,4 @@ class StopwatchApp(App):
 
 if __name__ == "__main__":
     app = StopwatchApp()
-    app.run
+    app.run()
